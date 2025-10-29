272278e2b7838a4f85cafe206e386cc44a03e100
Ryan Gosling <ryangosling@MacBook-Air-Ryan.local>
2025-10-29 16:41:47 +0600

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,12 @@ export const Home = () => {
     <Card>
       <Title level={2}>Добро пожаловать в блог!</Title>
       <Paragraph>
-        Это современное SPA-приложение, созданное с помощью React и Ant Design.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis quos
+        in qui iure, necessitatibus itaque, temporibus harum culpa nemo, aut
+        pariatur asperiores error? Soluta, sapiente rerum? Soluta earum
+        temporibus magnam nihil vero blanditiis voluptatibus asperiores quae,
+        perspiciatis praesentium, quis velit, amet saepe commodi consequuntur
+        cum quod cupiditate. Obcaecati, numquam velit.
       </Paragraph>
     </Card>
   );
